7ed904646a8453baa968a39a31a8a69e9b77d51e
dhruv <dhruv2073.be21@chitkara.edu.in>
2022-09-10 21:01:05 +0530

    done with day 9, made caesar cypher

--- Day-8/caesar_cypher.py
+++ Day-8/caesar_cypher.py
@@ -7,10 +7,6 @@ def caesar(start_text, shift_amount, cipher_direction):
   if cipher_direction == "decode":
     shift_amount *= -1
   for char in start_text:
-    #TODO-3: What happens if the user enters a number/symbol/space?
-    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-    #e.g. start_text = "meet me at 3"
-    #end_text = "•••• •• •• 3"
     if char in alphabet:
       position = alphabet.index(char)
       new_position = position + shift_amount
