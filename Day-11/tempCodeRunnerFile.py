5d6f251f8bd43da145098940e48d72a0c9de755f
dhruv <dhruv2073.be21@chitkara.edu.in>
2022-11-08 15:43:23 +0530

    made blackjack capstone project

---
+++ Day-11/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+  print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
+  print(compare(user_score, computer_score))
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    os.system('cls')
+    play_game()
